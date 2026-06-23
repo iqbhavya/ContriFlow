6c2af18a30eb1c82c8ed2d831e7317c5fc2262cc
Bhavya Yadav <guniverse2006@gmail.com>
2026-06-23 14:56:09 +0530

    Downgrade Prisma to v6 and configure Prisma client

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const authRoutes = require("./routes/auth.routes");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth", authRoutes);
+
+
+app.get("/", (req, res) => {
+  res.send("ContriFlow API Running");
+});
+
+
+app.listen(3000, () => {
+  console.log("Server Running");
+});
---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,53 @@
+const prisma = require("../lib/prisma");
+const bcrypt = require("bcryptjs");
+
+const signup  = async (req, res) => {
+    try{
+      const { name, email, password } = req.body;
+
+    if(!name || !email || !password) {
+        return res.status(400).json({ 
+            message: "All fields are required" });
+    } 
+
+    const existingUser = await prisma.user.findUnique({
+        where: { email },
+    });
+
+    if(existingUser) {
+        return res.status(400).json({
+            message: "User already exists" 
+        });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        },
+    });
+
+    res.status(201).json({
+        message: "User created successfully",
+        user: {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+        },
+    });
+    }catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" 
+        });
+    }
+};
+
+module.exports = {
+    signup,
+};
+
+
+
